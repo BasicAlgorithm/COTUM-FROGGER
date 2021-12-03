a033a1467622ab6e04875394196784172cf8daa5
Mateo Gonzales Navarrete <mgonzalesn17@outlook.com>
2021-12-03 17:37:05 -0500

    Fix collisions

--- Assets/_CotumFrogger/Scripts/CollisionWithSomeCar.cs
+++ Assets/_CotumFrogger/Scripts/CollisionWithSomeCar.cs
@@ -2,31 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
 
-public class CollisionWithSomeCar : MonoBehaviour
+public class CollisionWithSomeCar : MonoBehaviourPunCallbacks, IPunObservable
 {
-
-
     private void Start()
     {
     }
+
     private void Update()
     {
     }
-    private void OnCollisionEnter(Collision collision)
+
+    private void OnTriggerEnter(Collider collision)
     {
         // Temporal comment to test all work on scene FirstLevel
-        /*
-        if (collision.gameObject.tag == "SomeCar")
+        if (!photonView.IsMine)
         {
-            Debug.Log("YOU LOSE");
-            SceneManager.LoadScene("LoseRoom");
+            return;
         }
-        if (collision.gameObject.tag == "WinObject")
+
+        if (collision.tag.Contains("SomeCar"))
+        {
+            //gameObject.SetActive(false);
+            //Photon
+            Debug.Log("YOU LOSE");
+            //SceneManager.LoadScene("LoseRoom");
+        }
+        else if (collision.tag.Contains("WinObject"))
         {
             Debug.Log("YOU WIN");
-            SceneManager.LoadScene("MenuRoom");
+            //SceneManager.LoadScene("MenuRoom");
         }
-        */
     }
+
+    #region IPunObservable implementation
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        Debug.Log("CollisionWithSomeCar::OnPhotonSerializeView");
+    }
+
+    #endregion
 }
