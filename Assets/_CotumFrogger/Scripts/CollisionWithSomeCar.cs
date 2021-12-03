496214535745dd9d6cde872c70de2cd5ee443d71
Mateo Gonzales Navarrete <mgonzalesn17@outlook.com>
2021-12-03 17:55:09 -0500

    Spawn tombstone on death

--- Assets/_CotumFrogger/Scripts/CollisionWithSomeCar.cs
+++ Assets/_CotumFrogger/Scripts/CollisionWithSomeCar.cs
@@ -6,6 +6,10 @@ using Photon.Pun;
 
 public class CollisionWithSomeCar : MonoBehaviourPunCallbacks, IPunObservable
 {
+    [Tooltip("The Tombstone Prefab")]
+    [SerializeField]
+    private GameObject tombstonePrefab;
+
     private void Start()
     {
     }
@@ -24,8 +28,8 @@ public class CollisionWithSomeCar : MonoBehaviourPunCallbacks, IPunObservable
 
         if (collision.tag.Contains("SomeCar"))
         {
-            //gameObject.SetActive(false);
-            //Photon
+            gameObject.SetActive(false);
+            PhotonNetwork.Instantiate(this.tombstonePrefab.name, gameObject.transform.position, Quaternion.identity, 0);
             Debug.Log("YOU LOSE");
             //SceneManager.LoadScene("LoseRoom");
         }
